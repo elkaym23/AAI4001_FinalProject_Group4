0f2441dda2339fd95e7306a9abea9df870984316
lanna_lalala;- <147015359+Alalalallalalalalalalalal@users.noreply.github.com>
2026-02-21 19:17:09 -0500

    conversion

--- streamlit_frontend/AAI4001_FinalProject_Group4_deployment.py
+++ streamlit_frontend/AAI4001_FinalProject_Group4_deployment.py
@@ -489,14 +489,14 @@ with tab1:
                 prediction = response.json()["predicted_yield_hg_per_ha"]
 
                 st.markdown(f"""
-                <div class="metric-card">
-                    <h2>🌾 Predicted Crop Yield</h2>
-                    <h1>{prediction:,.2f} hg/ha</h1>
-                    <h1>{prediction * 0.001:,.2f} kg/a</h1>
-                    <h1>{prediction * 0.001 * 2.20462:,.2f} lb/a</h1>
-                    <p>{crop} — {year}</p>
-                </div>
-                """, unsafe_allow_html=True)
+                    <div class="metric-card">
+                        <h2>🌾 Predicted Crop Yield</h2>
+                        <h1>{prediction:,.2f} hg/ha</h1>
+                        <h1>{prediction * 0.04047:,.2f} kg/acre</h1>
+                        <h1>{prediction * 0.08921:,.2f} lb/acre</h1>
+                        <p>{crop} — {year}</p>
+                    </div>
+                    """, unsafe_allow_html=True)
 
             else:
                 st.error("❌ FastAPI request failed. Check your backend server.")
